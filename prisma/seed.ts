b4b9f1cee19d710f321aa63aac09d35869e0b3ad
tcmak217 <tcmak217@outlook.com>
2023-02-18 11:18:57 +0800

    feat: add a seed script to add dummy data to database

---
+++ prisma/seed.ts
@@ -0,0 +1,45 @@
+import { PrismaClient } from '.prisma/client';
+const prisma = new PrismaClient();
+
+async function main() {
+  const symbols = [
+    'btc',
+    'eth',
+    'ltc',
+    'xmr',
+    'xrp',
+    'doge',
+    'dash',
+    'maid',
+    'lsk',
+    'sicx',
+  ];
+
+  for (const [index, symbol] of symbols.entries()) {
+    const quote = await prisma.quote.upsert({
+      where: { id: index + 1 },
+      update: {
+        symbol: symbol,
+        price: Math.random() * 10000 + 10000,
+        volume: Math.random() * 10000 + 1000,
+        timestamp: new Date(),
+      },
+      create: {
+        symbol: symbol,
+        price: Math.random() * 10000 + 10000,
+        volume: Math.random() * 10000 + 1000,
+        timestamp: new Date(),
+      },
+    });
+    console.log({ quote });
+  }
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
