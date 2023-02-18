050ef9ee83ade0a2f9a9048889cf1ff2dac84e0d
tcmak217 <tcmak217@outlook.com>
2023-02-18 12:11:40 +0800

    feat: add prisma service
    The prisma service is for instantiating PrismaClient

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { QuotesModule } from './quotes/quotes.module';
 import { CronModule } from './cron/cron.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [QuotesModule, ScheduleModule.forRoot(), CronModule],
+  imports: [QuotesModule, ScheduleModule.forRoot(), CronModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
