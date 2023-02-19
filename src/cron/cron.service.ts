ca28ee12e59723bb7a502d798897d834d3e0470e
tcmak217 <tcmak217@outlook.com>
2023-02-19 17:31:46 +0800

    feat: change the API provider

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -3,14 +3,31 @@ import { Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { lastValueFrom, map } from 'rxjs';
 
+const cryptoName: string[] = [
+  'bitcoin',
+  'ethereum',
+  'litecoin',
+  'monero',
+  'ripple',
+  'xrp',
+  'dogecoin',
+  'dash',
+  'maidsafecoin',
+  'lisk',
+  'storjcoin-x',
+];
+
+const coincapAPI =
+  'https://api.coincap.io/v2/assets?ids=' + cryptoName.join(',');
+
 @Injectable()
 export class CronService {
   constructor(private readonly httpService: HttpService) {}
-  // Get quote from https://api.cryptonator.com/api/ticker/btc-usd every 10 seconds
+
   @Cron(CronExpression.EVERY_10_SECONDS)
   async getQuote() {
     const observable = await this.httpService
-      .get('https://api.cryptonator.com/api/ticker/btc-usd')
+      .get(coincapAPI)
       .pipe(map((response) => response.data));
     const data = await lastValueFrom(observable);
     console.log(data);
